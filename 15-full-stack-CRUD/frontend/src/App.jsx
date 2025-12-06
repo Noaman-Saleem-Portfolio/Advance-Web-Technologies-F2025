d8ad7510d8378db3484a5d1faa22b50db332de88
Noaman-Saleem-Portfolio <noaman2266@google.com>
2025-12-06 08:52:55 +0500

    Dashboard Added

---
+++ 15-full-stack-CRUD/frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route, Navigate } from "react-router"
+
+import Dashboard from "./pages/admin/dashboard/Dashboard"
+import Products from "./pages/admin/products/Products"
+import Users from "./pages/admin/users/Users"
+import Stats from "./pages/admin/stats/Stats"
+
+import Home from "./pages/user/home/Home"
+
+const App = () => {
+  return (
+    <Routes>
+      {/* user routes */}
+      <Route path="/" element={<Home />} />
+
+      {/* admin routes */}
+      <Route path="/admin" element={<Dashboard />} >
+        {/* When user visits /admin → redirect */}
+        <Route index element={<Navigate to="stats" replace />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="products" element={<Products />} />
+        <Route path="users" element={<Users />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
